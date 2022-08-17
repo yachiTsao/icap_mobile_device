8c8606550ccd29235ea02c4413512d832dee4a25
Ychi <ychi7799@gmail.com>
2022-08-17 10:43:12 +0800

    feat: complete alarm list and resolve function

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'page/alarm_page.dart';
 import 'page/device_page.dart';
 import 'page/setting_pade.dart';
 
-
-void main() => runApp(MaterialApp(
+void main() => runApp(
+      MaterialApp(
         debugShowCheckedModeBanner: false,
         home: MyApp(),
       ),
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   //目前選擇頁索引值
   int _currentIndex = 0; //預設值
 
-  final pages = [HomePage(), AlarmPage(), DevicePage() ,SettingPage()];
+  final pages = [DevicePage(), AlarmPage(),  SettingPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -35,27 +35,19 @@ class _MyAppState extends State<MyApp> {
         centerTitle: true,
       ),
       drawer: Drawer(
-        backgroundColor: Color.fromARGB(210, 47, 59, 82),
+        backgroundColor: Color.fromARGB(255, 47, 59, 82),
         child: ListView(
           children: <Widget>[
             //設定用户名稱
             UserAccountsDrawerHeader(
-               decoration: BoxDecoration(
-                  color: Color.fromARGB(210, 47, 59, 82),
-                ),
-              accountName: new Text(
-                "iCAP 使用者",
-                style: TextStyle(
-                  color: Color.fromARGB(250, 146, 164, 199)
-                )
+              decoration: BoxDecoration(
+                color: Color.fromARGB(210, 47, 59, 82),
               ),
+              accountName: Text("iCAP 使用者",
+                  style: TextStyle(color: Color.fromARGB(250, 146, 164, 199))),
               //設定Email
-              accountEmail: new Text(
-                "IPAIntern@gmail.com",
-                style: TextStyle(
-                  color: Color.fromARGB(250, 146, 164, 199)
-                )
-              ),
+              accountEmail: Text("IPAIntern@gmail.com",
+                  style: TextStyle(color: Color.fromARGB(250, 146, 164, 199))),
               //設定大頭照
               currentAccountPicture: new CircleAvatar(
                 backgroundColor: Color.fromARGB(250, 146, 164, 199),
@@ -63,52 +55,38 @@ class _MyAppState extends State<MyApp> {
             ),
             //選單
             ListTile(
-              leading: new CircleAvatar(child: Icon(Icons.home_rounded,
-              color: Color.fromARGB(249, 187, 209, 252)),
-              backgroundColor: Color.fromARGB(210, 47, 59, 82)),
-              title: Text('首頁',
-              style: TextStyle(
-                  color: Color.fromARGB(249, 187, 209, 252)
-                )
-              ),
+              leading: CircleAvatar(
+                  child: Icon(Icons.devices_rounded,
+                      color: Color.fromARGB(249, 187, 209, 252)),
+                  backgroundColor: Color.fromARGB(210, 47, 59, 82)),
+              title: Text('裝置列表',
+                  style: TextStyle(color: Color.fromARGB(249, 187, 209, 252))),
               onTap: () {
                 _onItemClick(0);
               },
             ),
             ListTile(
-              leading: new CircleAvatar(child: Icon(Icons.notifications_active_rounded,
-              color: Color.fromARGB(249, 187, 209, 252)),
-              backgroundColor: Color.fromARGB(210, 47, 59, 82)),
+              leading: CircleAvatar(
+                  child: Icon(Icons.notifications_active_rounded,
+                      color: Color.fromARGB(249, 187, 209, 252)),
+                  backgroundColor: Color.fromARGB(210, 47, 59, 82)),
               title: Text('通知列表',
-              style: TextStyle(
-                  color: Color.fromARGB(249, 187, 209, 252)
-                )),
+                  style: TextStyle(color: Color.fromARGB(249, 187, 209, 252))),
               onTap: () {
                 _onItemClick(1);
+                data = {};
+                dataArray = [];
               },
             ),
             ListTile(
-              leading: new CircleAvatar(child: Icon(Icons.devices_rounded,
-              color: Color.fromARGB(249, 187, 209, 252)),
-              backgroundColor: Color.fromARGB(210, 47, 59, 82)),
-              title: Text('裝置列表',
-              style: TextStyle(
-                  color: Color.fromARGB(249, 187, 209, 252)
-                )),
+              leading: CircleAvatar(
+                  child: Icon(Icons.settings_sharp,
+                      color: Color.fromARGB(249, 187, 209, 252)),
+                  backgroundColor: Color.fromARGB(210, 47, 59, 82)),
+              title: Text('設定',
+                  style: TextStyle(color: Color.fromARGB(249, 187, 209, 252))),
               onTap: () {
                 _onItemClick(2);
-              },
-            ),
-            ListTile(
-              leading: new CircleAvatar(child: Icon(Icons.settings_sharp,
-              color: Color.fromARGB(249, 187, 209, 252)),
-              backgroundColor: Color.fromARGB(210, 47, 59, 82)),
-              title: Text('設定',
-              style: TextStyle(
-                  color: Color.fromARGB(249, 187, 209, 252)
-                )),
-              onTap: () {
-                _onItemClick(3);
               },
             ),
           ],
@@ -125,4 +103,3 @@ class _MyAppState extends State<MyApp> {
     });
   }
 }
-
